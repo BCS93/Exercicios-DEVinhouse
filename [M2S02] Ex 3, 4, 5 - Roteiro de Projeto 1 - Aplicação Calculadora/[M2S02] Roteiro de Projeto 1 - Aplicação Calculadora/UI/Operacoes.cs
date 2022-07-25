878245e7a84c8d5d2d894ab9e0214b195a4c7e04
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-07-25 13:43:37 -0300

    Add files via upload

---
+++ [M2S02] Ex 3, 4, 5 - Roteiro de Projeto 1 - Aplicação Calculadora/[M2S02] Roteiro de Projeto 1 - Aplicação Calculadora/UI/Operacoes.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _M2S02__Roteiro_de_Projeto_1___Aplicação_Calculadora.UI
+{
+    public class Operacoes
+    {
+        public static void adicao()
+        {
+            double num1;
+            double num2;
+            double soma = 0;
+
+            Console.Clear();
+            Console.WriteLine(" Digite o primeiro número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n Digite o segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            soma = num1 + num2;
+            Console.WriteLine($"\n A soma dos números digitados é: {soma}");
+            Console.ReadKey();
+
+        }
+
+        public static void subtracao()
+        {
+            double num1;
+            double num2;
+            double subtracao = 0;
+
+            Console.Clear();
+            Console.WriteLine(" Digite o primeiro número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n Digite o segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            subtracao = num1 - num2;
+            Console.WriteLine($"\n A soma dos números digitados é: {subtracao}");
+            Console.ReadKey();
+
+        }
+
+        public static void multiplicacao()
+        {
+            double num1;
+            double num2;
+            double multiplicacao = 0;
+
+            Console.Clear();
+            Console.WriteLine(" Digite o primeiro número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n Digite o segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            multiplicacao = num1 * num2;
+            Console.WriteLine($"\n A soma dos números digitados é: {multiplicacao}");
+            Console.ReadKey();
+
+        }
+        public static void divisao()
+        {
+            double num1;
+            double num2;
+            double divisao = 0;
+
+            Console.Clear();
+            Console.WriteLine(" Digite o primeiro número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n Digite o segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            divisao = num1 / num2;
+            Console.WriteLine($"\n A soma dos números digitados é: {divisao}");
+            Console.ReadKey();
+
+        }
+
+    }
+}
