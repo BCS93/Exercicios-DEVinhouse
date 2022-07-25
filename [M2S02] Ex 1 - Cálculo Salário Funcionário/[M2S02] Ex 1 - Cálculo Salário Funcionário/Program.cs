49e49aba5ba690fec81f8b31296056c5c75497fa
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-07-25 12:35:32 -0300

    Add files via upload

---
+++ [M2S02] Ex 1 - Cálculo Salário Funcionário/[M2S02] Ex 1 - Cálculo Salário Funcionário/Program.cs
@@ -0,0 +1,30 @@
+﻿string nome;
+int anoDeNascimento;
+double salario;
+double reajuste;
+
+Console.WriteLine("\nDigite o nome do(a) funcionário(a): ");
+nome = Console.ReadLine();
+
+Console.WriteLine("\nDigite o ano de nascimento: ");
+anoDeNascimento = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nDigite o salário do(a) funcionário(a): ");
+salario = double.Parse(Console.ReadLine());
+
+Console.WriteLine("\nDigite o valor do reajuste (%): ");
+reajuste = double.Parse(Console.ReadLine());
+
+
+double salarioReajustado = salario + (salario * (reajuste/100));
+int idade =  DateTime.Now.Year - anoDeNascimento;
+
+Console.WriteLine("\nO salário reajustado do(a) funcionário(a) " + nome + ", de "+idade+" anos, é R$"+salarioReajustado);
+Console.ReadLine();
+
+
+
+
+
+
+
