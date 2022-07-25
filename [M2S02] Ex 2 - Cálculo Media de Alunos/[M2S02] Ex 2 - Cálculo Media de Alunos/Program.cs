9c8e4f9845771ef2df890f943944329e51b706a6
BCS93 <bruno_kuwabata@estudante.sesisenai.org.br>
2022-07-25 11:25:27 -0300

    Adicionar arquivos de projeto.

---
+++ [M2S02] Ex 2 - Cálculo Media de Alunos/[M2S02] Ex 2 - Cálculo Media de Alunos/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] notas = new double[6];
+double soma = 0;
+double media =0;
+int alunoID = 1;
+
+
+for(int n = 0; n<notas.Length ;n++)
+{   
+    Console.WriteLine($"qual a nota do aluno {alunoID++}?");
+    notas[n] = double.Parse(Console.ReadLine());
+
+    soma+= notas[n];
+
+    media = soma / notas.Length;
+}
+
+
+Console.WriteLine($"a nota média dos alunos é: {media}");
