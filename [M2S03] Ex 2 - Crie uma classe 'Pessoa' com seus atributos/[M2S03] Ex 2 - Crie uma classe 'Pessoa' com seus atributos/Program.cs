3fbb07706c32bb7939f948fa069ba28bfa244bff
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-07-26 18:07:21 -0300

    Add files via upload

---
+++ [M2S03] Ex 2 - Crie uma classe 'Pessoa' com seus atributos/[M2S03] Ex 2 - Crie uma classe 'Pessoa' com seus atributos/Program.cs
@@ -0,0 +1,7 @@
+﻿class Pessoa 
+{
+    private string Nome { get; set; } 
+    private DateTime DataDeNascimento { get; set; }
+    private double Altura { get; set; } 
+    private double Peso { get; set; }
+}
