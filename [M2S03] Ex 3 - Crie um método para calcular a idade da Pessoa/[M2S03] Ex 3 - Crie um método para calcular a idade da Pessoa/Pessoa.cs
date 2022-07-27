8c723b2f7a12da2259924de6106236b72d82df1b
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-07-27 20:16:13 -0300

    Add files via upload

---
+++ [M2S03] Ex 3 - Crie um método para calcular a idade da Pessoa/[M2S03] Ex 3 - Crie um método para calcular a idade da Pessoa/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _M2S03__Ex_3___Crie_um_método_para_calcular_a_idade_da_Pessoa
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int DataDeNascimento { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+
+
+        public static int CalcularIdade(Pessoa pessoa)
+        {
+            int calculoDaIdade = Convert.ToInt32(DateTime.Now.Year - pessoa.DataDeNascimento);
+
+            return calculoDaIdade;
+        }
+    }
+}
---
+++ [M2S03] Ex 3 - Crie um método para calcular a idade da Pessoa/[M2S03] Ex 3 - Crie um método para calcular a idade da Pessoa/Program.cs
@@ -0,0 +1,19 @@
+﻿using _M2S03__Ex_3___Crie_um_método_para_calcular_a_idade_da_Pessoa;
+
+Pessoa pessoa1 = new Pessoa();
+{
+    Console.WriteLine(" Digite o seu nome:");
+    pessoa1.Nome = Console.ReadLine();
+    Console.WriteLine("\n Digite o ano do seu nascimento: (ex:1995)");
+    pessoa1.DataDeNascimento = int.Parse(Console.ReadLine());
+    Console.WriteLine("\n Digite a sua altura:");
+    pessoa1.Altura = double.Parse(Console.ReadLine());
+    Console.WriteLine("\n Digite o seu peso: ");
+    pessoa1.Peso = double.Parse(Console.ReadLine());  
+    
+};
+
+int idade = Pessoa.CalcularIdade(pessoa1);
+
+Console.WriteLine($"\n A idade de {pessoa1.Nome} é {idade} anos");
+Console.ReadKey();
