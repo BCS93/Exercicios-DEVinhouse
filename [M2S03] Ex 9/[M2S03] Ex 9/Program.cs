b87d8f36ca364ccd156b300a71e1e5649364a875
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-07-27 21:09:55 -0300

    Add files via upload

---
+++ [M2S03] Ex 9/[M2S03] Ex 9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+class Adicao
+{
+    public static void Soma(double num1, double num2, [Optional] double num3, [Optional] double num4) 
+    { 
+        Console.WriteLine(num1+num2+num3+num4);
+    }
+
+
+    public static void Main()
+    {
+        Soma(2, 8);
+        Soma(2, 8, 9);
+        Soma(2, 8, 9, 30.3);
+
+    }
+}
+
