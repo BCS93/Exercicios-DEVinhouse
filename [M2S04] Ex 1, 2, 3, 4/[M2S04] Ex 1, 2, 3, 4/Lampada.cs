d6811b37ef0c22d20d2fd9e03e28c62428644038
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-08-02 12:39:56 -0300

    Add files via upload

---
+++ [M2S04] Ex 1, 2, 3, 4/[M2S04] Ex 1, 2, 3, 4/Lampada.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _M2S04__Ex_1__2__3__4
+{
+    public class Lampada
+    {
+
+        public int Potencia { get; set; }
+        public int Voltagem { get; set; }
+
+
+        public Lampada(int potencia, int voltagem)
+        {
+            Potencia = potencia;
+            Voltagem = voltagem;
+        }
+
+        public override string ToString()
+        {
+            return $"Potência: {Potencia} W\n" +
+                   $"Voltagem: {Voltagem} V ";
+        }
+
+        public static void Liga()
+        {
+            Console.WriteLine("A lâmpada ligou\n");
+            Console.ReadKey();
+        }
+
+        public static void Desliga()
+        {
+            Console.WriteLine("A lâmpada desligou\n");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ [M2S04] Ex 1, 2, 3, 4/[M2S04] Ex 1, 2, 3, 4/Program.cs
@@ -0,0 +1,87 @@
+﻿using _M2S04__Ex_1__2__3__4;
+
+bool interruptor = true;
+string estado = "desligada";
+
+Random rand = new Random();
+
+Lampada lampadaNova1 = new Lampada(7, 127);
+
+
+while (interruptor)
+{
+    Menu();
+    Console.WriteLine($"status: {estado}\n");
+    Console.WriteLine(lampadaNova1);
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.Clear();
+
+            if (estado == "ligada")
+            {
+                Console.WriteLine("A lâmpada já está ligada");
+                Console.ReadKey();
+            }
+            else if (estado == "queimada")
+            {
+                Console.WriteLine("A lâmpada não funciona mais.");
+                Console.ReadKey();
+            }
+            else
+            {
+                int probabilidade = rand.Next(1, 101);
+
+                if (probabilidade <= 15) // probabilidade de 15%
+                {
+                    Console.WriteLine("A lâmpada queimou");
+                    Console.ReadKey();
+                    estado = "queimada";
+                }
+                else
+                {
+                    Lampada.Liga();
+                    estado = "ligada";
+                }
+            }
+            break;
+
+        case "2":
+            Console.Clear();
+            if (estado == "desligada")
+            {
+                Console.WriteLine("A lâmpada já está desligada");
+                Console.ReadKey();
+            }
+            else if (estado == "queimada")
+            {
+                Console.WriteLine("A lâmpada não funciona mais.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Lampada.Desliga();
+                estado = "desligada";
+            }
+            break;
+
+        case "3":
+            Console.Clear();
+            interruptor = false;
+            break;
+
+        default:
+            Console.Clear();
+            Console.WriteLine("Opção Inválida");
+            Console.ReadKey();
+            break;
+
+    }
+}
+
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("(1)Ligar\n(2)Desligar\n(3)Sair\n");
+}
