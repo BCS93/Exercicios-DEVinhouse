6888d36209588954c8266a988e35034d0b47bc87
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-08-04 20:00:27 -0300

    Add files via upload

---
+++ [M2S04] Ex 10/Pessoa.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _M2S04__Ex_10
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public double IMC { get; set; }
+
+
+        public Pessoa(string nome, int idade, double peso, double altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Peso = peso;
+            Altura = altura;
+            IMC = 0;
+        }
+
+        public double CalculoIMC()
+        {
+            IMC = Peso / (Altura * Altura);
+
+            return IMC;
+        }
+
+        public string CategoriaIMC(double imc)
+        {
+            
+            if (imc < 18.5)
+            {
+                return "Abaixo do Peso";
+            }
+            else if (imc >= 18.5 && imc < 25)
+            {
+                return "Peso Normal";
+            }
+            else 
+            {
+                return "Acima do Peso";
+            }
+        }
+
+    }
+}
---
+++ [M2S04] Ex 10/Program.cs
@@ -0,0 +1,20 @@
+﻿using _M2S04__Ex_10;
+
+
+
+Pessoa pessoa1 = new Pessoa("Bruno", 29, 72.2, 1.70);
+Pessoa pessoa2 = new Pessoa("Alana", 27, 52.3, 1.58);
+Pessoa pessoa3 = new Pessoa("Kaito", 2, 14.2, 0.82);
+
+
+double imc = pessoa1.CalculoIMC();
+Console.WriteLine($"O cálculo IMC de {pessoa1.Nome}, de {pessoa1.Idade} anos, apontou que a sua categoria é: {pessoa1.CategoriaIMC(imc)}");
+
+
+imc = pessoa2.CalculoIMC();
+Console.WriteLine($"\nO cálculo IMC de {pessoa2.Nome}, de {pessoa2.Idade} anos, apontou que a sua categoria é: {pessoa2.CategoriaIMC(imc)}");
+
+
+imc = pessoa3.CalculoIMC();
+Console.WriteLine($"\nO cálculo IMC de {pessoa3.Nome}, de {pessoa3.Idade} anos, apontou que a sua categoria é: {pessoa3.CategoriaIMC(imc)}");
+Console.ReadKey();
