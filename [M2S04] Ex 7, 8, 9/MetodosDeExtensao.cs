0f51a179fe8100d948e661e04fe5705203dbe1da
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-08-04 17:37:10 -0300

    Add files via upload

---
+++ [M2S04] Ex 7, 8, 9/MetodosDeExtensao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _M2S04__Ex_7__8__9
+{
+    public static class MetodosDeExtensao
+    {
+
+        public static string TradutorDeBool(this bool valor)
+        {
+            return valor ? "O valor da variável é verdadeiro" : "O da valor variável é falso";
+        }
+
+        public static int Multiplicador(this int num) 
+        {
+            return num * num; 
+        }
+
+        public static decimal Multiplicador(this decimal num)
+        {
+            return num * num;
+        }
+
+        public static double Multiplicador(this int num1,double num2, double num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static double Multiplicador(this int num1, double num2, double num3=3.8, int num4 = 6)
+        {
+            return num1*num2*num3*num4;
+        }
+
+    }
+
+}
---
+++ [M2S04] Ex 7, 8, 9/Program.cs
@@ -0,0 +1,36 @@
+﻿using _M2S04__Ex_7__8__9;
+
+bool a = false;
+
+Console.WriteLine($"Quando 'a' possui o valor {a}, significa que: {a.TradutorDeBool()}");
+
+
+a = true;
+
+Console.WriteLine($"\nQuando 'a' possui o valor {a}, significa que: {a.TradutorDeBool()}\n\n"); 
+Console.ReadKey();
+
+
+int valor1 = 2;
+decimal valor2 = 2.78M;
+double valor3 = 2.5;
+
+
+//Método principal
+Console.WriteLine($"MÉTODO MULTIPLICADOR: {valor1} * {valor1} = {valor1.Multiplicador()}\n");
+
+
+//Sobrecargas:
+
+
+//receber decimal ao invés de int
+Console.WriteLine($"DECIMAL: {valor2} * {valor2} = {valor2.Multiplicador()}\n");
+
+
+//receber mais de 2 parâmetros
+Console.WriteLine($"DOIS OU MAIS PARÂMETROS: {valor1} * {valor3} * 5,5 = {valor1.Multiplicador(valor3, 5.5)}\n");
+
+
+//receber parâmetros opcionais
+Console.WriteLine($"PARÂMETROS OPCIONAIS: {valor1} * {valor3} * 2,5(opcional) * 2(opcional) = {valor1.Multiplicador(valor3, num3: 2.5, num4: 2)}\n");
+Console.ReadKey();
