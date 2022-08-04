cd26fb27d47ab4e13e054693fa5d1c403f00836f
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-08-03 21:53:14 -0300

    Add files via upload

---
+++ [M2S04] Ex 5 e 6/[M2S04] Ex 5 e 6/Conta.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _M2S04__Ex_5_e_6
+{
+    public class Conta
+    {
+        public string NomeDoCliente { get; set; }
+        public int NumeroDaConta { get; set; }
+        public double Saldo { get; set; }
+
+
+        public Conta(string nomeDoCliente, int numeroDaConta, double saldo)
+        {
+            NomeDoCliente = nomeDoCliente;
+            NumeroDaConta = numeroDaConta;
+            Saldo = saldo;
+        }
+
+        public void Depositar(double valor)
+        {
+            Console.Clear();
+            Console.WriteLine("Operação realizada com sucesso.");
+            Console.ReadKey();
+
+            Saldo += valor;
+        }
+        public void Sacar(double valor)
+        {
+            if (Saldo < valor)
+            {
+                Console.Clear();
+                Console.WriteLine("Seu saldo é insuficiente para realizar essa operação.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Operação realizada com sucesso.");
+                Console.ReadKey();
+
+                Saldo -= valor;
+            }
+
+
+        }
+
+        public double ObterSaldo()
+        {
+            return Saldo;
+        }
+
+        public int ObterNumeroConta()
+        {
+            return NumeroDaConta;
+        }
+
+        public string ObterNomeCliente()
+        {
+            return NomeDoCliente;
+        }
+
+        /*public override string ToString()
+        {
+            return $"{Saldo.ToString("C")}";
+        }*/
+    }
+
+
+}
---
+++ [M2S04] Ex 5 e 6/[M2S04] Ex 5 e 6/Program.cs
@@ -0,0 +1,80 @@
+﻿
+using _M2S04__Ex_5_e_6;
+
+bool interruptor = true;
+string opcao;
+string nome;
+int conta;
+double saldo = 0;
+
+Conta novaConta ;
+
+
+double valorDigitado;
+
+
+
+Cadastro();
+while (interruptor)
+{
+    Menu();
+    switch (opcao)
+    {
+        case "1":
+            Console.Clear();
+            Console.WriteLine("Qual o valor a ser depositado?");
+            valorDigitado = double.Parse(Console.ReadLine());
+            novaConta.Depositar(valorDigitado);
+            break;
+
+        case "2":
+            Console.Clear();
+            Console.WriteLine("Qual o valor a ser sacado?");
+            valorDigitado = double.Parse(Console.ReadLine());
+            novaConta.Sacar(valorDigitado);
+            break;
+        case "3":
+            Console.Clear();
+            Console.WriteLine(novaConta.ObterSaldo().ToString("C"));
+            Console.ReadKey();
+            break;
+        case "4":
+            Console.Clear();
+            Console.WriteLine($"Nome: {novaConta.ObterNomeCliente()} \nConta: {novaConta.ObterNumeroConta()}");
+            Console.ReadKey();
+            break;
+        case "5":
+            Console.Clear();
+            Console.WriteLine("Saindo...");
+            interruptor = false;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Opção Inválida");
+            Console.ReadKey();
+            break;
+
+    }
+}
+
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Selecione uma operação");
+    Console.WriteLine("\n(1)Depositar  \n(2)Sacar  \n(3)Ver o Saldo  \n(4)Ver Informações da Conta  \n(5)Sair ");
+    opcao = Console.ReadLine();
+
+}
+
+ void Cadastro()
+{
+    Console.WriteLine("Bem vindo, faça o login na sua conta.");
+
+    Console.WriteLine("\nDigite seu Nome: ");
+    nome = Console.ReadLine();
+
+    Console.WriteLine("\nDigite sua Conta: ");
+    conta = int.Parse(Console.ReadLine());
+
+    novaConta = new Conta(nome, conta, saldo);
+}
